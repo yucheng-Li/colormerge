6e2c497800ed15313a4204c180629bea9adcf70c
yucheng-Li <244065684@qq.com>
2021-03-03 20:28:45 +0800

    dsd

---
+++ src/page/colorthief/index.tsx
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react'
+
+interface Props {
+
+}
+
+export class Colorthief extends Component<Props, any> {
+    render() {
+        return (
+            <div className="colorthief">
+                this is colorthief
+            </div>
+        )
+    }
+}
---
+++ src/page/colorwheel/index.tsx
@@ -0,0 +1,15 @@
+import React,{ Component } from 'react'
+
+interface Props {
+
+}
+
+export default class Colorwheel extends Component<Props, any> {
+    render() {
+        return (
+            <div className="colorwheel">
+                this is colorwheel
+            </div>
+        )
+    }
+}
